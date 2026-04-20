7d9667c2b2177dd9da673de37bf093745451e92a
Wimlan <wilma.yasuda@gmail.com>
2026-04-20 11:34:27 +0200

    added search

--- TheatreShowsAPI/Endpoints/ShowEndpoints.cs
+++ TheatreShowsAPI/Endpoints/ShowEndpoints.cs
@@ -11,14 +11,40 @@ namespace TheatreShowsAPI.Endpoints
             app.MapGet("/shows/{id}", LoadShowById);
         }
 
-        private static IResult LoadAllShows(ShowData data) 
-        { 
-        return Results.Ok(data.Shows);
+        private static IResult LoadAllShows(
+            ShowData data,
+            string? showType,
+            string? search) 
+        {
+            var output = data.Shows;
+            if (string.IsNullOrWhiteSpace(showType) == false)
+            { 
+            output.RemoveAll(x=> string.Compare(
+                x.ShowType,
+                showType, 
+                StringComparison.OrdinalIgnoreCase) != 0);
+            }
+
+            if(string.IsNullOrWhiteSpace(search) == false)
+            {
+                output.RemoveAll(x => !x.Title.Contains(search, StringComparison.OrdinalIgnoreCase) &&
+                !x.Description.Contains(search, StringComparison.OrdinalIgnoreCase));
+            }
+
+
+        return Results.Ok(output);
         }
 
         private static IResult LoadShowById(ShowData data, int id)
         {
-            return Results.Ok(data.Shows.SingleOrDefault(s => s.Id == id));
-               }
+            var output = data.Shows.SingleOrDefault(s => s.Id == id);
+            if(output is null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(output);
+
+        }
     }
 }
--- TheatreShowsAPI/Models/ShowModel.cs
+++ TheatreShowsAPI/Models/ShowModel.cs
@@ -5,7 +5,8 @@
 
         public int Id { get; set; }
         public required string Title { get; set; }
-        public required string Description { get; set; } 
+        public required string Description { get; set; }
+        public required string ShowType { get; set; }
         public required string Venue { get; set; } 
         public int YearPublished { get; set; }
         public required string Image { get; set; } 
