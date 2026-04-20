decc44becdcc6f43c95bb886b9a58f26b2950028
Wimlan <wilma.yasuda@gmail.com>
2026-04-20 16:12:13 +0200

    added cors config

---
+++ SampleTestUI/Models/ShowModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleTestUI.Models
+{
+    public class ShowModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string ShowType { get; set; }
+        public required string Venue { get; set; }
+        public int YearPublished { get; set; }
+        public required string Image { get; set; }
+    }
+}
---
+++ TheatreShowsAPI/Startup/CorsConfig.cs
@@ -0,0 +1,28 @@
+﻿using Scalar.AspNetCore;
+
+namespace TheatreShowsAPI.Startup;
+
+public static class CorsConfig
+{
+    private const string SpecificOriginPolicy = "AllowSpecificOrigin";
+
+    public static void AddCorsServices(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(SpecificOriginPolicy, policy =>
+            {
+                policy
+                    .WithOrigins("https://localhost:7048", "https://wilmayasuda.se") // exact origin (scheme + host [+ :port])
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                    // .AllowCredentials(); // enable only if you need cookies/credentials
+            });
+        });
+    }
+
+    public static void ApplyCorsConfig(this WebApplication app)
+    {
+        app.UseCors(SpecificOriginPolicy);
+    }
+}
